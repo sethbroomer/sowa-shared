870fc6eb6a5642d292eae28d1ebb9a276471ac00
Seth Broomer <seth@dogvacay.com>
2013-10-13 14:39:01 -0700

    updated package and i don't know why these didn't get checked in

---
+++ index.js
@@ -0,0 +1,21 @@
+'use strict';
+
+var shared = {};
+
+
+
+shared.helpers = {
+    router       :  require('./helpers/router-middleware'),
+    BackboneBase :  require('./helpers/backbone-base')
+};
+
+shared.config = {
+    initalize : function(app, express, rootPath) {
+        return require('./config/config')(app, express, rootPath);
+    },
+    logger : require('./config/config-logging'),
+    router : require('./config/config-routers'),
+    view   : require('./config/config-views')
+};
+
+module.exports = shared;
